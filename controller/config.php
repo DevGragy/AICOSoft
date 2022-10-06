75d64ec31c901706089fa7dc9224354261fdd9a4
DevJayMx <eliasgarcia971@gmail.com>
2022-10-06 13:26:52 -0500

    BD en prod

--- controller/config.php
+++ controller/config.php
@@ -1,8 +1,11 @@
 <?php 
     $servidor = "localhost";
-    $usuario = "root";
-    $password = "";
-    $db = "aico";
+    // root
+    $usuario = "u175573487_root";
+    // 
+    $password = "Aicosoft1";
+    //aico
+    $db = "u175573487_aico";
     $conexion = new mysqli($servidor, $usuario, $password, $db);
 
     if($conexion->connect_error){
